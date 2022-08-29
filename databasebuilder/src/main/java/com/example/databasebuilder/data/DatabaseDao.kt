d2e6cdd2e5dd62125725b8270a947b51acd9a5b3
Tyler Williams <tylerlw1988@gmail.com>
2022-08-29 14:25:34 -0400

    Add dao file contents for database creation

--- databasebuilder/src/main/java/com/example/databasebuilder/data/DatabaseDao.kt
+++ databasebuilder/src/main/java/com/example/databasebuilder/data/DatabaseDao.kt
@@ -1,2 +1,20 @@
 package com.example.databasebuilder.data
 
+import androidx.room.*
+
+@Dao
+interface DatabaseDao {
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    suspend fun insertAlternate(pokemon: AlternateForm)
+
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    suspend fun insert(pokemon: Pokemon)
+
+    // Method for updating a row in the database.
+    @Update
+    suspend fun updatePokemonDatabase(pokemon: Pokemon)
+
+    // Query for generating the list of distinct evolution chain numbers.
+    @Query("SELECT DISTINCT evolutionChain FROM National")
+    suspend fun getChainNumberList(): List<Int>
+}
--- databasebuilder/src/main/java/com/example/databasebuilder/data/Pokemon.kt
+++ databasebuilder/src/main/java/com/example/databasebuilder/data/Pokemon.kt
@@ -1,2 +1,5 @@
 package com.example.databasebuilder.data
 
+class Pokemon {
+
+}

e3cf513dd97fc20a2522ca9b6779a864942533b7
Tyler Williams <tylerlw1988@gmail.com>
2022-08-29 14:39:31 -0400

    Add database application

--- databasebuilder/src/main/java/com/example/databasebuilder/DatabaseApplication.kt
+++ databasebuilder/src/main/java/com/example/databasebuilder/DatabaseApplication.kt
@@ -1,2 +1,8 @@
 package com.example.databasebuilder
 
+import android.app.Application
+import com.example.databasebuilder.data.DataRoomDatabase
+
+class DatabaseApplication : Application() {
+    val database: DataRoomDatabase by lazy { DataRoomDatabase.getDatabase(this) }
+}

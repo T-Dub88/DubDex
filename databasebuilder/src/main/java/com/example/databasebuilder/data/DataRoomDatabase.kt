af03eb7d3a168ab717a0cfcd6966095e0ba925fc
Tyler Williams <tylerlw1988@gmail.com>
2022-09-12 12:25:02 -0400

    Clean up databaseBuilder module files

--- databasebuilder/src/main/java/com/example/databasebuilder/data/DataRoomDatabase.kt
+++ databasebuilder/src/main/java/com/example/databasebuilder/data/DataRoomDatabase.kt
@@ -1,8 +1,6 @@
 package com.example.databasebuilder.data
 
-import android.content.Context
 import androidx.room.Database
-import androidx.room.Room
 import androidx.room.RoomDatabase
 
 @Database(entities = [Pokemon::class, AlternateForm::class], version = 1, exportSchema = false)
